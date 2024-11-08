74ded5fdc6e8fa2ec4835604d55d199f737243fc
cOwO7 <oclc00@naver.com>
2024-11-08 17:57:40 +0900

    세부 폴더 생성 및 main 작성

--- src/main/java/com/springbootfinal/app/SpringbootfinalApp03Application.java
+++ src/main/java/com/springbootfinal/app/SpringbootfinalApp03Application.java
@@ -1,4 +1,4 @@
-package com.springbootfinal_app;
+package com.springbootfinal.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
